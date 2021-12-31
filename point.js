066d070774f7d56558b4773e8c7c67b511b4dbc8
Wyess <diyhacker@gmail.com>
2021-12-31 21:29:46 +0900

    Re-written using a class

--- point.js
+++ point.js
@@ -1,53 +1,55 @@
-function Point(x, y) {
-  this.x = x;
-  this.y = y;
-}
-
-Point.prototype.pmove = function(r, deg) {
-  this.x += r * Math.cos(deg * Math.PI / 180);
-  this.y += r * Math.sin(deg * Math.PI / 180);
-  return this;
-};
-
-Point.prototype.move = function(dx, dy) {
-  this.x += dx;
-  this.y += dy;
-  return this;
-};
-
-Point.prototype.add = function(pt) {
-  this.x += pt.x;
-  this.y += pt.y
-  return this;
-};
-
-Point.prototype.scale = function(sx, sy) {
-  this.x *= sx;
-  if (typeof(sy) != 'undefined') {
-    this.y *= sy
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
-  return this;
-};
 
-Point.prototype.set = function(x, y) {
-  this.x = x;
-  this.y = y;
-  return this;
-};
+  pmove(r, deg) {
+    this.x += r * Math.cos(deg * Math.PI / 180);
+    this.y += r * Math.sin(deg * Math.PI / 180);
+    return this;
+  }
 
-Point.prototype.pset = function(r, deg) {
-  this.x = r * Math.cos(deg * Math.PI / 180);
-  this.y = r * Math.sin(deg * Math.PI / 180);
-  return this;
-};
+  move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+    return this;
+  }
 
-Point.prototype.toString = function() {
-  return this.x + "," + this.y;
-};
+  add(pt) {
+    this.x += pt.x;
+    this.y += pt.y
+    return this;
+  }
 
-Point.prototype.m = function() {
-  return "m" + this.x + "," + this.y;
-};
+  scale(sx, sy) {
+    this.x *= sx;
+    if (typeof(sy) != 'undefined') {
+      this.y *= sy
+    }
+    return this;
+  }
+
+  set(x, y) {
+    this.x = x;
+    this.y = y;
+    return this;
+  }
+
+  pset(r, deg) {
+    this.x = r * Math.cos(deg * Math.PI / 180);
+    this.y = r * Math.sin(deg * Math.PI / 180);
+    return this;
+  }
+
+  toString() {
+    return this.x + "," + this.y;
+  }
+
+  m() {
+    return "m" + this.x + "," + this.y;
+  }
+}
 
 p = function(x, y) {
   return new Point(x, y);
