705ebc20d1b2c6e2d2be8dfcf67fb22f2d72fed9
Wyess <diyhacker@mail.goo.ne.jp>
2022-01-22 17:34:22 +0900

    Changed to a module to add set_svg_to_pasteboard

--- util/pbsvg/pbsvg.py
+++ util/pbsvg/pbsvg.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
-from Cocoa import NSPasteboard, NSString, NSUTF8StringEncoding
+from Cocoa import NSPasteboard, NSString, NSUTF8StringEncoding, NSData
 
-pb = NSPasteboard.generalPasteboard()
-data = pb.dataForType_("image/svg+xml")
-svg = NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding)
-print(svg)
+__pb = NSPasteboard.generalPasteboard()
 
+def get_svg_from_pasteboard():
+    global __pb
+    data = __pb.dataForType_("image/svg+xml")
+    return NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding)
+
+def set_svg_to_pasteboard(svg):
+    global __pb
+    data = NSString.stringWithString_(svg).nsstring().dataUsingEncoding_(NSUTF8StringEncoding)
+    __pb.clearContents()
+    __pb.setData_forType_(data, "image/svg+xml")
+
+if __name__ == "__main__":
+    svg = get_svg_from_pasteboard()
+    if svg:
+        print(svg)
